1be7d1d033483aa9e881900ecff0fa470c7010e7
Kevin Ralph Lauren Paular <kevinralph.paular@stratpoint.com>
2025-09-22 03:24:33 +0800

    feat: set up navigation for login, register and home
    Refs: develop

--- app/src/main/java/com/assesment/gweather/MainActivity.kt
+++ app/src/main/java/com/assesment/gweather/MainActivity.kt
@@ -4,15 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.assesment.gweather.ui.auth.login.LoginScreen
+import com.assesment.gweather.ui.auth.register.RegisterScreen
+import com.assesment.gweather.ui.model.Screen
 import com.assesment.gweather.ui.theme.GWeatherTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             GWeatherTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AppNavigation()
                 }
             }
         }
@@ -31,14 +38,43 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController, startDestination = Screen.Login.route
+    ) {
+        composable(Screen.Login.route) {
+            LoginScreen(onNavigateToRegister = {
+                navController.navigate(Screen.Register.route)
+            }, onLoginSuccess = { user ->
+                navController.navigate(Screen.Home.route) {
+                    popUpTo(Screen.Login.route) { inclusive = true }
+                }
+            })
+        }
+
+        composable(Screen.Register.route) {
+            RegisterScreen(onRegisterSuccess = { user ->
+                navController.navigate(Screen.Home.route) {
+                    popUpTo(Screen.Register.route) { inclusive = true }
+                }
+            }, onNavigateBack = {
+                navController.popBackStack()
+            })
+        }
+
+        composable(Screen.Home.route) {
+            HomeScreen(
+                onLogout = {
+                    navController.navigate(Screen.Login.route) {
+                        popUpTo(0) // clears backstack
+                    }
+                })
+        }
+    }
 }
 
-@Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     GWeatherTheme {
